d009abbb821bb4264d025229b32ae80cdf8049e0
chamarno <chamard.arnaud@bbox.fr>
2016-05-19 15:20:38 +0200

    nettoyage index2 test
    update index racine

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p>&copy; <?php echo date('Y');?> - La Gastro' à Ginette - Tous droits réservés - Crédits et mentions légales</a>
+            </p>
+        </div>
+    </div>
+</footer>
