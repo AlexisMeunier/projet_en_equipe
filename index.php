1e771105f4d0518854bb3e53f73a96a9e1287968
YoanGarcia <yoan.gcia@hotmail.fr>
2016-05-20 10:12:23 +0200

    add index avec include

--- index.php
+++ index.php
@@ -1,56 +1,5 @@
 <?php require_once 'inc/connect.php';?>
-
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title> Notre resto bon et sympa</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
-    
-    <!-- Latest compiled and minified CSS -->
-    <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.0.0/normalize.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
- 
-    <link type="text/css" href="css/style.css" rel="stylesheet">
-
-
-</head>
-<body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <div class="infoResto">
-	                <a href="index.php"><h2><em> La Gastro' à Ginette  <em></h2></a>
-	               	 <!-- fonction ou code pour gérer coordonnées du Resto -->
-               	</div>
-
-            </div>
-            <div class="menuNav">
-                        <a href="contact.php"> Nous contacter </a>
-            </div>
-        </div>
-    </nav>
+<?php include_once 'inc/header.php'; ?>
 
     <section class="sectionCove">
 		<div id="myCarousel" class="carousel slide centerSlide" data-ride="carousel">
@@ -104,21 +53,5 @@
 	            <?php endforeach;?>
 	        </div><!--/jumbotron-->
 	    </div><!--/div container jumbotron-->
-        <!-- Footer -->
-    <div style="text-align:center"><?php include_once 'inc/footer.php';?>
-    </div>
-    <!-- /.container -->
-	</section><!-- /section3recettes -->
-    <!-- jQuery -->
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script   src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <script>
-    $('.carousel').carousel({
-        interval: 4500 //vitesse caroussel
-    })
-    </script>
-</body>
-</html>
+
+<?php include_once 'inc/footer.php'; ?>
