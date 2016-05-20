18c53ae0ff2000d0057e4907740f2ef369bac7e3
AlexisMeunier <meunier_33@live.fr>
2016-05-20 16:18:23 +0200

    edit infos update

--- admin/edit_infos.php
+++ admin/edit_infos.php
@@ -1,76 +1,46 @@
-<!--
- *
- *  Appuyer sur un bouton modifier les coordonnées du restaurants pour arriver sur cette page
- *
--->
-
-<h2>Modifier les infos du restaurant</h2>
-
-<hr><br><br>
-
-<!-- ***************************************************************************************** -->
-
 <?php
+	require_once '../inc/connect.php';
 
-require_once '../inc/connect.php';
+	$post = array();
+	$error = array();
+	$idInfos = 1;
 
-$post = array();
-$error = array();
-$displayErr = false;
-$formValid = false;
- 
-if(isset($_GET['id']) && !empty($_GET['id'])) {
-	$idInfos = $_GET['id']; 
-	if(!is_numeric($idInfos)) {
-		$idInfos = 1;
-	}
-}
+	if(!empty($_POST)){
+		$post = array_map('trim', array_map('strip_tags', $_POST));
 
-
-if(isset($idInfos)) {
-
-	if(!empty($_POST)) {
-
-		foreach($_POST as $key => $value) {
-			$post[$key] = trim(strip_tags($value));
-		}
-
-		if(strlen($post['name']) < 2 || strlen($post['name']) > 50) {
+		if(strlen($post['name']) < 2 || strlen($post['name']) > 50){
 			$error[] = 'Le nom du restaurant doit comporter entre 2 et 50 caractères';
 		}
 
-		if(!empty($post['address'])) {
+		if(empty($post['address'])){
 			$error[] = 'L\'adresse du restaurant doit être indiquée';
 		}
 
-		if(!empty($post['phone'])) {
+		if(empty($post['phone'])){
 			$error[] = 'Le téléphone du restaurant doit être indiqué';
 		}
 
-		if(count($error) > 0) {
-			$displayErr = true;
-		}
+		if(count($error) == 0){
+			$res = $bdd->prepare('UPDATE infos SET name = :name, address = :address, phone = :phone WHERE id = :id');
 
-		else {
-							
-			$res = $bdd->prepare('UPDATE infos SET name = :nameInfos, link = :linkArticle, phone = :contentArticle WHERE id = :idInfos');
-
-			$res->bindValue(':idInfos', intval($idInfos), PDO::PARAM_INT); 
-			$res->bindValue(':nameInfos', $post['name']);
-			$res->bindValue(':addressInfos', $post['address']);
-			$res->bindValue(':phoneInfos', $post['phone']);
+			$res->bindValue(':id', intval($idInfos), PDO::PARAM_INT); 
+			$res->bindValue(':name', $post['name']);
+			$res->bindValue(':address', $post['address']);
+			$res->bindValue(':phone', $post['phone']);
 
 			if($res->execute()) {
-				$formValid = true;
-							}
-			else {
 
+			}
+			else{
 				die(print_r($res->errorInfo()));
 			}
-
 		}
-
+		else{
+			$error[] = 'erreur lors de l\'update';				
+		}
 	}
+	// ('SELECT * FROM recette INNER JOIN users ON recette.users_id = users.id');
+
 
 	$res = $bdd->prepare('SELECT * FROM infos WHERE id = :idInfos');
 	$res->bindParam(':idInfos', $idInfos, PDO::PARAM_INT);
@@ -78,36 +48,31 @@ if(isset($idInfos)) {
 
 	$infosResto = $res->fetch(PDO::FETCH_ASSOC);
 
-	if($formValid) {
-		echo '<p class="txtgreen">Les infos du restaurant sont à jour</p>';
-	}
-
-	if($displayErr) {
-	echo '<div style="color:red">';
-	echo implode('<br>', $error);
-	echo '</div>';
-	}
-
 ?>
 
-<!-- ********************************************************************************************* -->
+<?php if(count($error) > 0): ?>
+	<p>
+		il y a des erreurs:<br>
+		- <?=implode('<br>-', $error);?>
+	</p>
+<?php endif; ?>
 
-<form method="POST">
-	<label for="name">Nom du restaurant</label>
-	<input type="text" id="name" name="name" value="<?php echo $infosResto['name'];?>">
+<?php if(isset($infosResto)): ?>
+	<form method="POST">
+		<label for="name">Nom du restaurant</label>
+		<input type="text" id="name" name="name" value="<?=$infosResto['name'];?>">
 
-	<br>
-	<label for="address">Adresse du restaurant</label>
-	<input type="text" id="address" name="address" value="<?php echo $infosResto['address'];?>">
+		<br>
+		<label for="address">Adresse du restaurant</label>
+		<input type="text" id="address" name="address" value="<?=$infosResto['address'];?>">
 
-	<br>
-	<label for="phone">Téléphone du restaurant</label>
-	<input type="phone" id="phone" name="phone" value="<?php echo $infosResto['phone'];?>">
+		<br>
+		<label for="phone">Téléphone du restaurant</label>
+		<input type="phone" id="phone" name="phone" value="<?=$infosResto['phone'];?>">
 
-	<br>
-	<input type="submit" value="Modifier">
-</form>
-
-<?php } ?>
-
-<?php include_once 'inc/footer.php'; ?>
+		<br>
+		<input type="submit" value="Modifier">
+	</form>
+<?php else: ?>
+	<p>aucune infos</p>
+<?php endif; ?>
