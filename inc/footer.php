3c22cd59c3aead36488b4b7e7c3615c6a69c169b
chamarno <chamard.arnaud@bbox.fr>
2016-05-20 16:23:02 +0200

    BIG ADD !!
    wazAAAAAAAAAAAAAAAA!!!! ;-)

--- inc/footer.php
+++ inc/footer.php
@@ -1,17 +1,12 @@
-	    <div style="text-align:center">
 
-		    <footer>
+		    <footer id="foot" class="text-center">
 	            <p>&copy; <?php echo date('Y');?> - La Gastro' à Ginette - Tous droits réservés - Crédits et mentions légales
 	            </p>
 			</footer>
+	   
+	    <script   src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script><!-- jQuery -->
 
-	    </div>
-
-	    <!-- jQuery -->
-	    <!-- Bootstrap core JavaScript ================================================== -->
-	    <script   src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
-
-	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script> <!-- Bootstrap core JavaScript ================================================== -->
 
 	</body>
 
--- inc/header.php
+++ inc/header.php
@@ -22,7 +22,6 @@
 	    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
 	    
 	    <!-- Latest compiled and minified CSS -->
-	    <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.0.0/normalize.min.css">
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
