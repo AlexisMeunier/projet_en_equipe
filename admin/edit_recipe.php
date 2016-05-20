2d11dc77d0a17c426c2557a970c18ee415b694a7
Sebblet <seb.blet@hotmail.fr>
2016-05-20 12:34:41 +0200

    edit_recipe.php

--- admin/edit_recipe.php
+++ admin/edit_recipe.php
@@ -1,7 +1,71 @@
 <?php 
 session_start();
+require_once '../inc/connect.php';
+
+$errors = array();
+$showErr = false;
+$success = false;
 
 // nettoyage des donnéess passées en get
+if(isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id'])){
+
+	$recipeId = intval($_GET['id']);
+
+	$selectRecipe = $bdd->prepare('SELECT * FROM recipes WHERE id = :recipeId');
+	$selectRecipe->bindValue(':recipeId', $recipeId, PDO::PARAM_INT);
+	$selectRecipe->execute();
+
+	$recipe = $selectRecipe->fetch(PDO::FETCH_ASSOC);
+}
+
+if($recipe['id'] != $recipeId){
+	$errors[] = 'Aucune recette trouvée';
+}
+
+if(count($errors) > 0){
+	$showErr = true;
+}
+
+/*if(isset($recipeId)){
+
+	if(!empty($_POST)){ // si il n' y a pas d'erreur dans l'upload du fichier 
+
+		$post = array_map('trim',array_map('strip_tags',$_POST));
+
+		if(strlen($post['title']) < 4 || strlen($post['title']) > 80){
+			$errors[] = 'Le titre doit comporter entre 4 et 80 caractères';
+		}
+		if(empty($post['content'])){
+			$errors[] = 'Le contenu de la recette est vide';
+		}
+
+		if(count($errors) > 0){
+			$showErr = true;
+
+		} else { // si il n'y a pas eu d'erreurs dans le traitement du form
+
+			$insert = $bdd->prepare('INSERT INTO recipes (title, content, picture, date_add, user_id) VALUES (:title, :content, :picture, NOW(), :userId)');
+			$insert->bindValue(':title', $post['title']);
+			$insert->bindValue(':content', $post['content']);
+			$insert->bindValue(':picture', $filepath);
+			$insert->bindValue(':userId', $userId);
+
+				if($insert->execute()){
+					$success = true;
+				} else {
+					die(print_r($insert->errorInfo()));
+				}
+			}
+		}
+	} else { // sinon, si l'internaute est déconnecté, on le redirige vers la page de connexion
+
+	header('Location:index.php');
+	die;
+}
+
+
+}*/
+
 
 // Select de la recette en fonction de son id passée en get
 
@@ -27,15 +91,24 @@ require_once '../inc/connect.php';
 
 	<h2>Editer une recette</h2>
 
+	<?php
+	if($showErr){
+		echo implode('<br>', $errors);
+	}
+	if($success){
+		echo 'La recette a bien été ajoutée';
+	}
+	?>
+
 	<form method="POST" class="well" enctype="multipart/form-data">
 		<div class="form-group">
 			<label for="title">Titre</label>
-			<input name="title" class="form-control" placeholder="Entrez le titre de votre recette">
+			<input name="title" class="form-control" <?php if(isset($recipe)){ echo 'value="'.$recipe['title'].'"';}?> placeholder="Entrez le titre de votre recette">
 		</div>
 
 		<div class="form-group">
 			<label for="content">Contenu</label>
-			<textarea name="content" class="form-control" placeholder="Tapez votre recette"></textarea>
+			<textarea name="content" class="form-control" placeholder="Tapez votre recette"><?php if(isset($recipe)){ echo $recipe['content'];}?></textarea>
 		</div>
 
 		<div class="form-group">
