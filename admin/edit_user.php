c714b8fb76580a140457d32a7657e296732bad75
AlexisMeunier <meunier_33@live.fr>
2016-05-23 13:53:25 +0200

    aze

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -2,8 +2,11 @@
 session_start();
 require_once '../inc/connect.php';
 
+if($_SESSION['user']['role'] != 'admin'){
+	header('location:index.php');
+}
+
 $errors = array();
-$showErr = false;
 
 // nettoyage des donnéess passées en get
 if(isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id'])){
@@ -18,24 +21,20 @@ if(isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id'])){
 	$users = $selectUsers->fetch(PDO::FETCH_ASSOC);
 }
  // si aucun id ne correspond
-if($users['id'] != $usersId){
+if($selectUsers->rowCount() == 0){
 	$errors[] = 'Aucune utilisateur trouvé';
-}
-
-if(count($errors) > 0){
-	$showErr = true;
 }
 
 // si $users[role]=admin, ok on poursuit, sinon retour à l'index admin//
 
 
 //traitement du formulaire pour mise à jour infos users role admin
-if(!empty($_POST) && !$showErr){ // s'il n' y a pas d'erreur et post non vide
+if(!empty($_POST)){ // s'il n' y a pas d'erreur et post non vide
 
 	$post = array_map('trim',array_map('strip_tags',$_POST));
 
 	if(preg_match('#^[A-Z]{1}.{2,30}$#', $post['lastname']) == 0){
-		$error[] = 'Le nom doit comporter entre 2 et 30 caractères';
+		$errors[] = 'Le nom doit comporter entre 2 et 30 caractères';
 	}
 
 	if(preg_match('#^[A-Z]{1}.{2,30}$#', $post['firstname']) == 0){
@@ -51,17 +50,11 @@ if(!empty($_POST) && !$showErr){ // s'il n' y a pas d'erreur et post non vide
 	}
 
 	if(count($errors) > 0){
-		$showErr = true;
-
 	} else { // si il n'y a pas eu d'erreurs dans le traitement du form
 
 		//requête UPDATE 
-		$upd = $bdd->prepare('UPDATE users SET 
-			lastname = :lastname,  
-			firstname = :firstname, 
-			email = :email,
-			password = :password WHERE id = :usersId');
-		$upd->bindValue(':usersId', $usersId);
+		$upd = $bdd->prepare('UPDATE users SET lastname = :lastname, firstname = :firstname, email = :email, password = :password WHERE id = :usersId');
+		$upd->bindValue(':usersId', $usersId, PDO::PARAM_INT);
 		$upd->bindValue(':lastname', $post['lastname']);
 		$upd->bindValue(':firstname', $post['firstname']);
 		$upd->bindValue(':email', $post['email']);
@@ -83,11 +76,14 @@ include_once 'inc/header.php';
 
 <h2>Modifier les informations de l'utilisateur</h2>
 
-<?php
-if($showErr){
-	echo implode('<br>', $errors);
-}
-?>
+<?php if(count($errors) > 0): ?>
+	<section id="section_errors" class="alert alert-danger">
+		<p>
+			Il y a des erreurs:<br>
+			- <?=implode('<br>-', $errors)?>
+		</p>
+	</section>
+<?php endif; ?>
 
 <form method="POST" class="well">
 	<div class="form-group">
